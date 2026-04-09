4f95a4377713ba17dab4c17299bdc330a1167eac
thorshan <myothunaing18@gmail.com>
2026-04-09 10:21:30 +0630

    ADD_VERCEL_ANALYTIC

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
 import { UserProvider } from "./providers/UserProvider";
+import { Analytics } from "@vercel/analytics/react";
 
 const App = () => {
   return (
     <UserProvider>
       <BrowserRouter>
         <AppRoutes />
+        <Analytics />
       </BrowserRouter>
     </UserProvider>
   );
